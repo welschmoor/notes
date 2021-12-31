587f3ef20e72eeacd0346822c987c3e5b6e7e469
Johannes Welsch <welschmoor@gmail.com>
2021-12-31 01:43:58 +0100

    added testing environment for Cypress and a controller

--- controllers/notes.js
+++ controllers/notes.js
@@ -53,7 +53,7 @@ notesRouter.post('/', async (request, response) => {
   const body = request.body
   const token = getTokenFrom(request)
   const decodedToken = jwt.verify(token, process.env.SECRET)
-  console.log("<><><><>",decodedToken)
+  
   if (!token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
@@ -63,7 +63,7 @@ notesRouter.post('/', async (request, response) => {
     content: body.content,
     important: body.important === undefined ? false : body.important,
     date: new Date(),
-    user: user._id
+    user: user._id,
   })
 
   const savedNote = await note.save()
--- models/note.js
+++ models/note.js
@@ -1,11 +1,5 @@
 const mongoose = require('mongoose')
 
-// const noteSchema = new mongoose.Schema({
-//   content: { type: String, required: true, minlength: 5 },
-//   date: { type: Date, required: true,
-//   },
-//   important: Boolean,
-// })
 
 const noteSchema = new mongoose.Schema({
   content: {
